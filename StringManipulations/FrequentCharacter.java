f233097f49935b1d8fd853a3733736e74c70e128
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-06-27 12:46:33 +0530

    Character with maximum frequency

---
+++ StringManipulations/FrequentCharacter.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class FrequentCharacter {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a String to find the character frequency");
+    String str = sc.nextLine().toLowerCase().trim();
+    sc.close();
+
+    // calling frequency method , passed str an argument
+    frequency(str);
+
+}
+public static void frequency(String str) {
+    int[] arr = new int[256];
+    int max = 0;
+    char maxChar =' ';
+    boolean multipleMax = false;  
+
+    for (int i = 0; i < str.length(); i++) {
+        
+        char ch = str.charAt(i);
+        arr[ch]++;
+
+        if (arr[ch] > max) {
+            max = arr[ch];
+            maxChar = ch;
+            multipleMax = false;  
+        } else if (arr[ch] == max) {
+            multipleMax = true;  
+        }
+    }
+
+    if (!multipleMax) { 
+        System.out.println(maxChar + " max frequency is " + max);
+    } else {
+        System.out.println("There are no characters with a distinct maximum frequency.");
+    }
+}
+}
